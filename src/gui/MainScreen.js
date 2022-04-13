c04867dff5bcf34f613a5242f8e754e339dfd534
Rodrigo Alfonso <rodri042@gmail.com>
2022-04-12 21:22:08 -0300

    Adding CodeMirror

--- src/gui/MainScreen.js
+++ src/gui/MainScreen.js
@@ -1,8 +1,21 @@
 import React, { PureComponent } from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
 import styles from "./MainScreen.module.css";
 
 export default class MainScreen extends PureComponent {
 	render() {
-		return <div className={styles.message}>Hello World!</div>;
+		return (
+			<div className={styles.message}>
+				<CodeMirror
+					value="console.log('hello world!');"
+					height="200px"
+					extensions={[javascript({ jsx: true })]}
+					onChange={(value, viewUpdate) => {
+						console.log("value:", value);
+					}}
+				/>
+			</div>
+		);
 	}
 }
