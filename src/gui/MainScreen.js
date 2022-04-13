40b62962d752b9b36378a722592a0a27a5634d7d
Rodrigo Alfonso <rodri042@gmail.com>
2022-04-12 21:35:53 -0300

    Adding pixijs

--- src/gui/MainScreen.js
+++ src/gui/MainScreen.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from "react";
+import * as PIXI from "pixi.js";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import { oneDark } from "@codemirror/theme-one-dark";
@@ -20,8 +21,34 @@ export default class MainScreen extends PureComponent {
 						console.log("value:", value);
 					}}
 				/>
-				<div className={classNames(styles.pane, styles.preview)}>holis</div>
+				<div id="preview" className={classNames(styles.pane, styles.preview)} />
 			</div>
 		);
+	}
+
+	componentDidMount() {
+		// The application will create a renderer using WebGL, if possible,
+		// with a fallback to a canvas render. It will also setup the ticker
+		// and the root stage PIXI.Container
+		const app = new PIXI.Application({
+			resizeTo: window,
+			backgroundAlpha: 0.5,
+		});
+		const graphics = new PIXI.Graphics();
+		app.stage.addChild(graphics);
+
+		let a = 1;
+		app.ticker.add(function (delta) {
+			graphics.clear();
+
+			// Rectangle
+			graphics.beginFill(0xff0000);
+			graphics.drawRect(50 + ++a, 50, 100, 100);
+			graphics.endFill();
+		});
+
+		// The application will create a canvas element for you that you
+		// can then insert into the DOM
+		document.querySelector("#preview").appendChild(app.view);
 	}
 }
