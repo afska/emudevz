550dc986cc7b230d68897e6d3005e9136f1b6f1b
Rodrigo Alfonso <rodri042@gmail.com>
2022-04-12 21:28:48 -0300

    Panes and dark theme

--- src/gui/MainScreen.js
+++ src/gui/MainScreen.js
@@ -1,20 +1,26 @@
 import React, { PureComponent } from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
+import { oneDark } from "@codemirror/theme-one-dark";
+import classNames from "classnames";
 import styles from "./MainScreen.module.css";
 
 export default class MainScreen extends PureComponent {
 	render() {
 		return (
-			<div className={styles.message}>
+			<div className={styles.container}>
 				<CodeMirror
+					className={styles.pane}
 					value="console.log('hello world!');"
-					height="200px"
-					extensions={[javascript({ jsx: true })]}
+					width="100%"
+					height="50vh"
+					theme={oneDark}
+					extensions={[javascript({})]}
 					onChange={(value, viewUpdate) => {
 						console.log("value:", value);
 					}}
 				/>
+				<div className={classNames(styles.pane, styles.preview)}>holis</div>
 			</div>
 		);
 	}
