677f84d052951ab045b52d65d91c86cdc40ba003
Rodrigo Alfonso <rodri042@gmail.com>
2022-04-12 21:58:21 -0300

    Removing bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./gui/theme/bootswatch/theme.css";
-import "./gui/theme/bootswatch/custom.css";
-import "./gui/theme/bootswatch/global.css";
 import "./gui/theme/crt.css";
 import "./index.css";
 
