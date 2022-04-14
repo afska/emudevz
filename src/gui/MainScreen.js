ecdd97bd23094d739e5e6342d4694aa739a368c7
Rodrigo Alfonso <rodri042@gmail.com>
2022-04-14 06:01:19 -0300

    Inverting components to understand the XTerm problem

--- src/gui/MainScreen.js
+++ src/gui/MainScreen.js
@@ -42,7 +42,7 @@ export default class MainScreen extends PureComponent {
 							this.setState({ selected: "top" });
 						}}
 					>
-						<TV />
+						<Terminal />
 					</div>
 
 					<div
@@ -55,7 +55,7 @@ export default class MainScreen extends PureComponent {
 							this.setState({ selected: "bottom" });
 						}}
 					>
-						<Terminal />
+						<TV />
 					</div>
 				</div>
 			</div>
