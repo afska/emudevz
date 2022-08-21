e8057a93094783b1b0906c3b3d6cfe53c8f6e3fe
Rodrigo Alfonso <rodri042@gmail.com>
2022-08-21 06:09:30 -0300

    Adding simple chat program

--- src/gui/terminal/commands/ChatCommand.js
+++ src/gui/terminal/commands/ChatCommand.js
@@ -1,4 +1,5 @@
 import Command from "./Command";
+import locales from "../../locales";
 import { theme } from "../style";
 
 const SPEED = 10;
@@ -9,17 +10,42 @@ export default class ChatCommand extends Command {
 	}
 
 	async execute(args) {
-		await this._terminal.writeln(
-			">> hi I'm a test message 😋  writing text 📙  very slow",
-			theme.MESSAGE,
-			SPEED
-		);
-		await this._terminal.writeln(">> what's your name?", theme.MESSAGE, SPEED);
-		const name = await this._terminal.prompt("?? ", theme.INPUT);
-		await this._terminal.writeln(
-			">> ok then, hi " + name + "!",
-			theme.MESSAGE,
-			SPEED
-		);
+		let state = "main";
+
+		while (state !== "end") {
+			const content = window.scr[state][locales.language];
+
+			for (let message of content.messages)
+				await this._terminal.writeln(`>> ${message}`, theme.MESSAGE, SPEED);
+
+			await this._terminal.newline();
+			await this._terminal.writeln("Choose an answer.", theme.SYSTEM);
+
+			const options = content.responses.map((rawResponse, responseId) => {
+				const number = parseInt(responseId) + 1;
+				const responseParts = rawResponse.split(/\[(\w+)\]/);
+				const response = responseParts[0];
+				const responseLink = responseParts[1];
+
+				return {
+					number,
+					response,
+					responseLink,
+				};
+			});
+
+			for (let option of options)
+				await this._terminal.writeln(`${option.number}) ${option.response}`);
+
+			let selectedOption = null;
+			while (selectedOption == null) {
+				const response = await this._terminal.prompt("?? ", theme.INPUT);
+				selectedOption = options.find(
+					(it) => it.number.toString() === response
+				);
+			}
+
+			state = selectedOption.responseLink;
+		}
 	}
 }
