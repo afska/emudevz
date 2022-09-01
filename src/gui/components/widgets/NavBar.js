f1af0a6ddd4b95b25153f7a9ccfd26d2ea461cea
Rodrigo Alfonso <rodri042@gmail.com>
2022-09-01 18:29:31 -0300

    Adjusting navbar size

--- src/gui/components/widgets/NavBar.js
+++ src/gui/components/widgets/NavBar.js
@@ -2,13 +2,14 @@ import React, { PureComponent } from "react";
 import ProgressList from "./ProgressList";
 import IconButton from "./IconButton";
 import { FaChevronLeft } from "react-icons/fa";
+import classNames from "classnames";
 import styles from "./NavBar.module.css";
 
 export default class NavBar extends PureComponent {
 	render() {
 		return (
 			<div className={styles.navbar}>
-				<div className={styles.item}>
+				<div className={classNames(styles.item, styles.text)}>
 					<IconButton Icon={FaChevronLeft} tooltip="Go back" />
 					<span>Chapter 1: Getting Started</span>
 				</div>
