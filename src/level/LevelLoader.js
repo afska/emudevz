7a7c49c804bd6cc8b542288bf001a29356e5cd08
Rodrigo Alfonso <rodri042@gmail.com>
2022-09-06 05:15:35 -0300

    FIX: Waiting for all chat scripts to load

--- src/level/LevelLoader.js
+++ src/level/LevelLoader.js
@@ -22,16 +22,18 @@ export default class LevelLoader {
 		const meta = JSON.parse(rawMeta);
 
 		const chatScripts = {};
-		LANGUAGES.forEach(async (language) => {
-			const chatFilePath = `${CHAT_FOLDER}/${language}.${CHAT_EXTENSION}`;
-			const chatFile = zip.file(chatFilePath);
-			if (!chatFile) throw new Error(`Missing file: ${chatFilePath}`);
-			const rawChat = await chatFile.async("string");
-			const chat = YAML.parse(rawChat);
-			const chatScript = new ChatScript(chat);
-			chatScript.validate();
-			chatScripts[language] = chatScript;
-		});
+		await Promise.all(
+			LANGUAGES.map(async (language) => {
+				const chatFilePath = `${CHAT_FOLDER}/${language}.${CHAT_EXTENSION}`;
+				const chatFile = zip.file(chatFilePath);
+				if (!chatFile) throw new Error(`Missing file: ${chatFilePath}`);
+				const rawChat = await chatFile.async("string");
+				const chat = YAML.parse(rawChat);
+				const chatScript = new ChatScript(chat);
+				chatScript.validate();
+				chatScripts[language] = chatScript;
+			})
+		);
 
 		const level = new Level(meta, chatScripts);
 		level.validate();
