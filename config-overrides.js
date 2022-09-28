ef92bef7f1f633203e5147ca9bf398a1723e6079
Rodrigo Alfonso <rodri042@gmail.com>
2022-09-27 22:46:39 -0300

    Simplifying polyfills as they're not needed anymore for 6502tools

--- config-overrides.js
+++ config-overrides.js
@@ -2,7 +2,7 @@ module.exports = function override(config) {
 	const fallback = config.resolve.fallback || {};
 	Object.assign(fallback, {
 		fs: require.resolve("./polyfills/fs"),
-		process: require.resolve("process/browser"),
+		process: require.resolve("./polyfills/process"),
 	});
 	config.resolve.fallback = fallback;
 
