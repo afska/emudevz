7d833a60f4e8980294efc92f0797d64521f947df
Rodrigo Alfonso <rodri042@gmail.com>
2022-10-05 08:37:22 -0300

    Finishing 007_Addressing modes (3-3)

--- src/data/levels/001_Assembly language/006_Addressing modes (2-3)/tests/test.js
+++ src/data/levels/001_Assembly language/006_Addressing modes (2-3)/tests/test.js
@@ -10,7 +10,7 @@ it("$407F contains $00", () => {
 	cpu.memory.readAt(0x407f).should.equal(0x00);
 });
 
-it("all bytes from $4080 to $40BF contain $aa", () => {
+it("all bytes from $4080 to $40BF contain $AA", () => {
 	const addresses = [...Array(64).keys()].map((it) => 0x4080 + it);
 	addresses.forEach((address) => {
 		cpu.memory.readAt(address).should.equal(0xaa);
--- src/data/levels/001_Assembly language/007_Addressing modes (3-3)/code/content.asm
+++ src/data/levels/001_Assembly language/007_Addressing modes (3-3)/code/content.asm
@@ -1,15 +1,15 @@
-LDA #$CD
+; Indexed indirect:
+LDY #$80
+STY $03
+LDY #$40
+STY $04
 
-; Zero Page,X
+LDA #$FA
 LDX #$02
-STA $60,X ; Writes [A] to $62
-INX
-STA $60,X ; Writes [A] to $63
+STA ($01,X) ; = STA ($01+X) = STA ($0003) = STA $4080
+;   ^ dereferences to $4080
 
-; Zero Page,X (wrap)
-LDX #$06
-STA $FF,X ; Writes [A] to $05
-
-; Absolute,X
-LDX #$02
-STA $4050,X ; Writes [A] to $4052
+; Indirect indexed:
+LDY #$5
+LDA ($03),Y ; = LDA ($03)+Y = LDA $4080+Y = LDA $4085
+;   ^ dereferences to $4085
