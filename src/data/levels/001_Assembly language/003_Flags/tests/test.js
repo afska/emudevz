dd003a8785d683e52c43cf728e71df158a3c2bce
Rodrigo Alfonso <rodri042@gmail.com>
2022-10-07 01:34:43 -0300

    Fixing levels

--- src/data/levels/001_Assembly language/003_Flags/tests/test.js
+++ src/data/levels/001_Assembly language/003_Flags/tests/test.js
@@ -10,12 +10,12 @@ beforeEach(() => {
 });
 
 it("it doesn't change the initial code", () => {
-	bytes.length.should.be.at.least(4);
-	bytes.slice(0, 4).should.eql(new Uint8Array([0xa9, 0xc8, 0x69, 0x3c]));
+	bytes.length.should.be.at.least(5);
+	bytes.slice(0, 5).should.eql(new Uint8Array([0xa2, 0xc8, 0x8a, 0x69, 0x3c]));
 });
 
-it("it only uses 3 instructions", () => {
-	instructions.length.should.equal(3);
+it("it only uses 4 instructions", () => {
+	instructions.length.should.equal(4);
 });
 
 it("it uses SBC", () => {
