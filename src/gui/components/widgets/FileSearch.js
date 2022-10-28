bfc96c4b615f3381e555bcf48538f567bf84df09
Rodrigo Alfonso <rodri042@gmail.com>
2022-10-28 06:57:56 -0300

    Styling file list

--- src/gui/components/widgets/FileSearch.js
+++ src/gui/components/widgets/FileSearch.js
@@ -9,8 +9,23 @@ export default class FileSearch extends PureComponent {
 
 		return (
 			<div className={classNames(styles.container, className)} {...rest}>
-				{/* <input type="text" /> */}
-				<Form.Control type="email" placeholder="Enter email" />
+				<Form.Control
+					placeholder="Enter a file name..."
+					spellCheck={false}
+					className={styles.input}
+				/>
+
+				<div className={styles.results}>
+					<div className={styles.result}>CPU.js</div>
+					<div className={classNames(styles.result, styles.selected)}>
+						C<span className={styles.highlight}>artr</span>idge.js
+					</div>
+					<div className={styles.result}>
+						cpu/instructions/b<span className={styles.highlight}>ranch</span>
+						ing.js
+					</div>
+					<div className={styles.result}>ppu/sprites/oam.js</div>
+				</div>
 			</div>
 		);
 	}
