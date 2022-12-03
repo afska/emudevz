cc730fccc197eaf4f34beced9f0f820b340f1a11
Rodrigo Alfonso <rodri042@gmail.com>
2022-12-03 07:29:39 -0300

    Saving volume in local storage

--- src/gui/sound/music.js
+++ src/gui/sound/music.js
@@ -3,7 +3,7 @@ const TRACKS = ["pl1sk3n.mp3", "m3m0r13s.mp3", "3muwh4t.mp3"];
 
 class Music {
 	constructor() {
-		this._volume = 0.5;
+		this._volume = this._loadVolume();
 		this._audio = new Audio("music/pl1sk3n.mp3");
 		this._hasStarted = false;
 	}
@@ -15,6 +15,7 @@ class Music {
 	set volume(value) {
 		this._volume = value;
 		this._audio.volume = value;
+		this._saveVolume();
 	}
 
 	start() {
@@ -22,6 +23,17 @@ class Music {
 
 		this._audio.play();
 		this._hasStarted = true;
+	}
+
+	_loadVolume() {
+		const value = parseInt(localStorage.getItem("volume"));
+		if (isFinite(value) && value >= 0 && value <= 1) return value;
+
+		return 0.5;
+	}
+
+	_saveVolume() {
+		localStorage.setItem("volume", this._volume);
 	}
 }
 
