51e8ff4aff6ec88d04a7d70634ea2178c183e623
Rodrigo Alfonso <rodri042@gmail.com>
2023-01-11 21:59:45 -0300

    Adding 'Little Endian' level

--- src/data/levels/$tests/cpu.js
+++ src/data/levels/$tests/cpu.js
@@ -549,3 +549,51 @@ it("the stack reads RAM and increments [SP] on pop", () => {
 	},
 	use: ({ id }, book) => id >= book.getId("4.8"),
 });
+
+// 4.9 Little Endian
+
+it("can read 16-bit values from the memory bus", () => {
+	const cpu = newCPU([0x34, 0x12]);
+
+	cpu.memory.write(0x0050, 0x45);
+	cpu.memory.write(0x0051, 0x23);
+
+	cpu.memory.should.respondTo("read16");
+	cpu.memory.read16(0x0050).should.equal(0x2345);
+	cpu.memory.read16(0x8000).should.equal(0x1234);
+})({
+	locales: {
+		es: "puede leer valores de 16 bits del bus de memoria",
+	},
+	use: ({ id }, book) => id >= book.getId("4.9"),
+});
+
+it("can push 16-bit values onto the stack", () => {
+	const cpu = newCPU();
+
+	cpu.stack.should.respondTo("push16");
+	cpu.stack.push16(0x1234);
+
+	cpu.stack.pop().should.equal(0x34);
+	cpu.stack.pop().should.equal(0x12);
+})({
+	locales: {
+		es: "puede poner valores de 16 bits en la pila",
+	},
+	use: ({ id }, book) => id >= book.getId("4.9"),
+});
+
+it("can pop 16-bit values from the stack", () => {
+	const cpu = newCPU();
+
+	cpu.stack.push(0x12);
+	cpu.stack.push(0x34);
+
+	cpu.stack.should.respondTo("pop16");
+	cpu.stack.pop16().should.equal(0x1234);
+})({
+	locales: {
+		es: "puede sacar valores de 16 bits de la pila",
+	},
+	use: ({ id }, book) => id >= book.getId("4.9"),
+});
