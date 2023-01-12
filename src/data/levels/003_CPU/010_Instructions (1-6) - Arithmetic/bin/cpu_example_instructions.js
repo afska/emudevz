fbad0cccfa8708a5e7d35cb437ccd0ee83744507
Rodrigo Alfonso <rodri042@gmail.com>
2023-01-12 07:41:50 -0300

    Adding an exercise emoji to ALL exercises

--- src/data/levels/003_CPU/010_Instructions (1-6) - Arithmetic/bin/cpu_example_instructions.js
+++ src/data/levels/003_CPU/010_Instructions (1-6) - Arithmetic/bin/cpu_example_instructions.js
@@ -19,12 +19,28 @@ export default {
       cpu.flags.updateZeroAndNegative(newValue);
     },
   },
-  LDA: {
+  ADC: {
     argument: "value",
     run(cpu, val) {
-      // Loads <val> into [A], updating the Z and N flags.
-      cpu.a.setValue(val);
-      cpu.flags.updateZeroAndNegative(val);
+      // Adds the contents of <val> to [A] together with the Carry Flag
+      // ([A] = [A] + <val> + C), updating the Z and N flags.
+      const oldValue = cpu.a.getValue();
+      const result = oldValue + val + cpu.flags.c;
+      const newValue = byte.toU8(result);
+      cpu.a.setValue(newValue);
+      cpu.flags.updateZeroAndNegative(newValue);
+
+      // C and V flags are set in case of unsigned and signed overflow respectively.
+      // Unsigned overflow occurs when the result is >= `256` (use `byte.overflows(...)`)
+      // Signed overflow occurs when `Positive + Positive = Negative` or `Negative + Negative = Positive`
+      cpu.flags.c = byte.overflows(result);
+      cpu.flags.v =
+        (byte.isPositive(oldValue) &&
+          byte.isPositive(val) &&
+          byte.isNegative(newValue)) ||
+        (byte.isNegative(oldValue) &&
+          byte.isNegative(val) &&
+          byte.isPositive(newValue));
     },
   },
 };
--- src/data/levels/003_CPU/010_Instructions (1-6) - Arithmetic/code/copy.js
+++ src/data/levels/003_CPU/010_Instructions (1-6) - Arithmetic/code/copy.js
@@ -1,3 +1,3 @@
-["cpu_example_instructions.js"].forEach((file) => {
+["cpu_example_instructions.js", "cpu_instructions.en.md"].forEach((file) => {
 	filesystem.write(`${Drive.DOCS_DIR}/${file}`, level.bin[file]);
 });
