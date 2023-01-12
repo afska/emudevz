97580a491a4b84b970fbd4a0d92640fff3af1e28
Rodrigo Alfonso <rodri042@gmail.com>
2023-01-12 06:55:52 -0300

    Converting embed js files to spaces

--- src/data/levels/003_CPU/010_Instructions (1-6) - Arithmetic/bin/cpu_example_instructions.js
+++ src/data/levels/003_CPU/010_Instructions (1-6) - Arithmetic/bin/cpu_example_instructions.js
@@ -1,30 +1,30 @@
 import byte from "/lib/byte";
 
 export default {
-	INX: {
-		argument: "no",
-		run(cpu) {
-			// Increments [X], updating the Z and N flags.
-			cpu.x.increment();
-			cpu.flags.updateZeroAndNegative(cpu.x.getValue());
-		},
-	},
-	INC: {
-		argument: "address",
-		run(cpu, addr) {
-			// Adds one to the value held at <addr>, updating the Z and N flags.
-			const value = cpu.memory.read(addr);
-			const newValue = byte.toU8(value + 1);
-			cpu.memory.write(addr, newValue);
-			cpu.flags.updateZeroAndNegative(newValue);
-		},
-	},
-	LDA: {
-		argument: "value",
-		run(cpu, val) {
-			// Loads <val> into [A], updating the Z and N flags.
-			cpu.a.setValue(val);
-			cpu.flags.updateZeroAndNegative(val);
-		},
-	},
+  INX: {
+    argument: "no",
+    run(cpu) {
+      // Increments [X], updating the Z and N flags.
+      cpu.x.increment();
+      cpu.flags.updateZeroAndNegative(cpu.x.getValue());
+    },
+  },
+  INC: {
+    argument: "address",
+    run(cpu, addr) {
+      // Adds one to the value held at <addr>, updating the Z and N flags.
+      const value = cpu.memory.read(addr);
+      const newValue = byte.toU8(value + 1);
+      cpu.memory.write(addr, newValue);
+      cpu.flags.updateZeroAndNegative(newValue);
+    },
+  },
+  LDA: {
+    argument: "value",
+    run(cpu, val) {
+      // Loads <val> into [A], updating the Z and N flags.
+      cpu.a.setValue(val);
+      cpu.flags.updateZeroAndNegative(val);
+    },
+  },
 };
