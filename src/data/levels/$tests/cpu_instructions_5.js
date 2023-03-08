c793726140554c382f41c22cfe5199312ffe9171
Rodrigo Alfonso <rodri042@gmail.com>
2023-03-08 00:59:18 -0300

    Splitting instruction tests into 6 files

---
+++ src/data/levels/$tests/cpu_instructions_5.js
@@ -0,0 +1,34 @@
+const { evaluate, byte } = $;
+
+let mainModule;
+beforeEach(async () => {
+	mainModule = await evaluate();
+});
+
+const newHeader = (prgPages = 1, chrPages = 1) => {
+	// prettier-ignore
+	return [0x4e, 0x45, 0x53, 0x1a, prgPages, chrPages, 0b00000000, 0b00000000, 0, 0, 0, 0, 0, 0, 0, 0];
+};
+
+const newRom = (prgBytes = []) => {
+	const header = newHeader();
+	const prg = prgBytes;
+	const chr = [];
+	for (let i = prgBytes.length; i < 16384; i++) prg.push(0);
+	for (let i = 0; i < 8192; i++) chr.push(byte.random());
+	const bytes = new Uint8Array([...header, ...prg, ...chr]);
+
+	return bytes;
+};
+
+const newCPU = (prgBytes = null) => {
+	const CPU = mainModule.default.CPU;
+	const Cartridge = mainModule.default.Cartridge;
+
+	const cartridge =
+		prgBytes == null ? { prg: () => [] } : new Cartridge(newRom(prgBytes));
+
+	return new CPU(cartridge);
+};
+
+// TODO: IMPLEMENT
