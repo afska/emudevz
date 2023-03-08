dd400a5c240c31ddfbfb5880ac18092aa9b5a3de
Rodrigo Alfonso <rodri042@gmail.com>
2023-03-08 00:30:20 -0300

    Adding spanish version of cpu_instructions.md

--- src/data/levels/003_CPU/010_Instructions (1-6) - Arithmetic/code/copy.js
+++ src/data/levels/003_CPU/010_Instructions (1-6) - Arithmetic/code/copy.js
@@ -1,3 +1,7 @@
-["cpu_example_instructions.js", "cpu_instructions.en.md"].forEach((file) => {
+[
+	"cpu_example_instructions.js",
+	"cpu_instructions.en.md",
+	"cpu_instructions.es.md",
+].forEach((file) => {
 	filesystem.write(`${Drive.DOCS_DIR}/${file}`, level.bin[file]);
 });
