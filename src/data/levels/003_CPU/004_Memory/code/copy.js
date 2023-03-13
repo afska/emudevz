1efa4d43cb11738e09e4e911f0afc78334412d82
Rodrigo Alfonso <rodri042@gmail.com>
2023-03-13 07:39:17 -0300

    Moving CPU docs to docs/cpu

--- src/data/levels/003_CPU/004_Memory/code/copy.js
+++ src/data/levels/003_CPU/004_Memory/code/copy.js
@@ -1,3 +1,7 @@
+try {
+	filesystem.mkdir(`${Drive.DOCS_DIR}/cpu`);
+} catch (e) {}
+
 ["cpu_memory.en.md", "cpu_memory.es.md"].forEach((file) => {
-	filesystem.write(`${Drive.DOCS_DIR}/${file}`, level.bin[file]);
+	filesystem.write(`${Drive.DOCS_DIR}/cpu/${file}`, level.bin[file]);
 });
--- src/data/levels/003_CPU/010_Instructions (1-5) - Arithmetic/code/copy.js
+++ src/data/levels/003_CPU/010_Instructions (1-5) - Arithmetic/code/copy.js
@@ -1,7 +1,5 @@
-[
-	"cpu_example_instructions.js",
-	"cpu_instructions.en.md",
-	"cpu_instructions.es.md",
-].forEach((file) => {
-	filesystem.write(`${Drive.DOCS_DIR}/${file}`, level.bin[file]);
-});
+["example_instructions.js", "instructions.en.md", "instructions.es.md"].forEach(
+	(file) => {
+		filesystem.write(`${Drive.DOCS_DIR}/cpu/${file}`, level.bin[file]);
+	}
+);
