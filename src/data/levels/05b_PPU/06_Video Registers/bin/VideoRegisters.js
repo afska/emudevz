de3fa9ff3f366070c4c82a86c26581079fee74b6
Rodrigo Alfonso <rodri042@gmail.com>
2024-10-30 18:37:08 +0100

    Adding intro to scrolling

--- src/data/levels/05b_PPU/06_Video Registers/bin/VideoRegisters.js
+++ src/data/levels/05b_PPU/06_Video Registers/bin/VideoRegisters.js
@@ -54,6 +54,10 @@ class OAMData extends InMemoryRegister.PPU {
 }
 
 class PPUScroll extends InMemoryRegister.PPU {
+  onLoad() {
+    /* TODO: IMPLEMENT */
+  }
+
   onWrite(value) {
     /* TODO: IMPLEMENT */
   }
