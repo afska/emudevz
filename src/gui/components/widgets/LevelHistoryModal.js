653f25ca17c5f28d3952ca414cbb76838e44ab61
Rodrigo Alfonso <rodri042@gmail.com>
2025-02-28 01:03:56 -0300

    Highlighting current level in level history

--- src/gui/components/widgets/LevelHistoryModal.js
+++ src/gui/components/widgets/LevelHistoryModal.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from "react";
 import Modal from "react-bootstrap/Modal";
 import { connect } from "react-redux";
+import classNames from "classnames";
+import Level from "../../../level/Level";
 import locales from "../../../locales";
 import Button from "./Button";
 import styles from "./LevelHistoryModal.module.css";
@@ -27,11 +29,15 @@ class LevelHistoryModal extends PureComponent {
 							const chapter = book.getChapterOf(it);
 							if (!chapter) return false;
 							const levelDefinition = book.getLevelDefinitionOf(it);
+							const isCurrent = levelDefinition.id === Level.current.id;
 
 							return (
 								<Button
 									key={i}
-									className={styles.level}
+									className={classNames(
+										styles.level,
+										isCurrent && styles.current
+									)}
 									onClick={() => goTo(it)}
 								>
 									<span>{chapter.name[locales.language]}</span>
