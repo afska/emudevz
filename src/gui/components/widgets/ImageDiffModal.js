549c54e662b982879d35ebdc04fbf91d777fa39b
Rodrigo Alfonso <rodri042@gmail.com>
2025-03-09 04:20:09 -0300

    Adding react-image-diff and monkey-patching it to work with React 17

--- src/gui/components/widgets/ImageDiffModal.js
+++ src/gui/components/widgets/ImageDiffModal.js
@@ -1,8 +1,13 @@
 import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
 import Modal from "react-bootstrap/Modal";
-import _ from "lodash";
 import locales from "../../../locales";
 import styles from "./ImageDiffModal.module.css";
+
+// HACK: Monkey-patching React to have PropTypes (`react-image-diff` is very old and needs this)
+React.PropTypes = PropTypes;
+const ImageDiff = require("react-image-diff");
+React.PropTypes = undefined;
 
 export default class ImageDiffModal extends PureComponent {
 	render() {
@@ -20,7 +25,16 @@ export default class ImageDiffModal extends PureComponent {
 				<Modal.Header>
 					<Modal.Title>🔍 {locales.get("check_diffs")}</Modal.Title>
 				</Modal.Header>
-				<Modal.Body>{imageUrls?.new}</Modal.Body>
+				<Modal.Body>
+					{isOpen && (
+						<ImageDiff
+							before={imageUrls.old}
+							after={imageUrls.new}
+							type="difference"
+							value={0.5}
+						/>
+					)}
+				</Modal.Body>
 			</Modal>
 		);
 	}
