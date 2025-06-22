d077d0f51a6b086068e4022d16562ec8a01fc7d0
Rodrigo Alfonso <rodri042@gmail.com>
2025-06-22 01:14:14 -0300

    Refactoring two-column layouts

--- src/gui/components/debugger/Debugger_CPU.js
+++ src/gui/components/debugger/Debugger_CPU.js
@@ -2,111 +2,84 @@ const ImGui = window.ImGui;
 
 export default class Debugger_CPU {
 	draw() {
-		// compute widths of table1 and table2 so we can center them with a gap
-		const style = ImGui.GetStyle();
-		const headers1 = ["[A]", "[X]", "[Y]", "[SP]", "[PC]"];
-		const headers2 = ["N", "V", "-", "-", "D", "I", "Z", "C"];
-		let table1W = 0,
-			table2W = 0;
+		const registers = ["[A]", "[X]", "[Y]", "[SP]", "[PC]"];
+		const flags = ["N", "V", "-", "-", "D", "I", "Z", "C"];
 
-		for (let h of headers1)
-			table1W += ImGui.CalcTextSize(h).x + style.CellPadding.x * 2;
-		table1W +=
-			style.ItemInnerSpacing.x * (headers1.length - 1) +
-			style.FrameBorderSize * 2;
-		for (let h of headers2)
-			table2W += ImGui.CalcTextSize(h).x + style.CellPadding.x * 2;
-		table2W +=
-			style.ItemInnerSpacing.x * (headers2.length - 1) +
-			style.FrameBorderSize * 2;
+		const flagsSmall =
+			ImGui.TableFlags.SizingStretchProp |
+			ImGui.TableFlags.RowBg |
+			ImGui.TableFlags.Borders;
 
-		const availW = ImGui.GetContentRegionAvail().x;
-		const gap = style.ItemSpacing.x;
-		const offset = (availW - table1W - table2W - gap) * 0.5;
-		ImGui.SetCursorPosX(ImGui.GetCursorPosX() + offset);
+		ImGui.Columns(2, "cpu_flag_columns", false);
 
-		{
-			const flags =
-				ImGui.TableFlags.SizingFixedFit |
-				ImGui.TableFlags.RowBg |
-				ImGui.TableFlags.Borders |
-				ImGui.TableFlags.NoHostExtendX;
-			if (ImGui.BeginTable("table1", headers1.length, flags)) {
-				for (let h of headers1) {
-					ImGui.TableSetupColumn(h, ImGui.TableColumnFlags.WidthFixed);
-				}
-				ImGui.TableHeadersRow();
-				ImGui.TableNextRow();
-				for (let i = 0; i < headers1.length; i++) {
-					ImGui.TableSetColumnIndex(i);
-					ImGui.Text(i === 4 ? "$8000" : "$00");
-				}
-				ImGui.EndTable();
+		if (ImGui.BeginTable("registers", registers.length, flagsSmall)) {
+			for (let h of registers) {
+				ImGui.TableSetupColumn(h, ImGui.TableColumnFlags.None);
 			}
+			ImGui.TableHeadersRow();
+			ImGui.TableNextRow();
+			for (let i = 0; i < registers.length; i++) {
+				ImGui.TableSetColumnIndex(i);
+				ImGui.Text(i === 4 ? "$8000" : "$00");
+			}
+			ImGui.EndTable();
 		}
 
-		ImGui.SameLine(undefined, gap);
+		ImGui.NextColumn();
 
-		{
-			const flags =
-				ImGui.TableFlags.SizingFixedFit |
-				ImGui.TableFlags.RowBg |
-				ImGui.TableFlags.Borders |
-				ImGui.TableFlags.NoHostExtendX;
-			if (ImGui.BeginTable("table2", headers2.length, flags)) {
-				for (let h of headers2) {
-					ImGui.TableSetupColumn(h, ImGui.TableColumnFlags.WidthFixed);
-				}
-				ImGui.TableHeadersRow();
-				ImGui.TableNextRow();
-				for (let i = 0; i < headers2.length; i++) {
-					ImGui.TableSetColumnIndex(i);
-					ImGui.Text(i === 3 ? "1" : "0");
-				}
-				ImGui.EndTable();
+		if (ImGui.BeginTable("flags", flags.length, flagsSmall)) {
+			for (let h of flags) {
+				ImGui.TableSetupColumn(h, ImGui.TableColumnFlags.None);
 			}
+			ImGui.TableHeadersRow();
+			ImGui.TableNextRow();
+			for (let i = 0; i < flags.length; i++) {
+				ImGui.TableSetColumnIndex(i);
+				ImGui.Text(i === 3 ? "1" : "0");
+			}
+			ImGui.EndTable();
 		}
 
-		{
-			const flags =
-				ImGui.TableFlags.SizingFixedFit |
-				ImGui.TableFlags.RowBg |
-				ImGui.TableFlags.Borders |
-				ImGui.TableFlags.ScrollY |
-				ImGui.TableFlags.NoHostExtendX;
-			if (ImGui.BeginTable("table3", 4, flags)) {
-				ImGui.TableSetupScrollFreeze(0, 1);
-				ImGui.TableSetupColumn("[PC]", ImGui.TableColumnFlags.WidthFixed);
-				ImGui.TableSetupColumn(
-					"Binary",
-					ImGui.TableColumnFlags.WidthFixed,
-					10 * 8
-				);
-				ImGui.TableSetupColumn(
-					"Instruction",
-					ImGui.TableColumnFlags.WidthFixed,
-					16 * 8
-				);
-				ImGui.TableSetupColumn("Context", ImGui.TableColumnFlags.WidthFixed);
-				ImGui.TableHeadersRow();
+		ImGui.Columns(1);
 
-				for (let row = 0; row < 32; row++) {
-					ImGui.TableNextRow();
-					for (let col = 0; col < 4; col++) {
-						ImGui.TableSetColumnIndex(col);
-						ImGui.Text(
-							col === 0
-								? "C000"
-								: col === 1
-								? "4C F5 C5"
-								: col === 2
-								? "JMP $C5F5"
-								: "A:00 X:00 Y:00 P:24 SP:FD CYC:7"
-						);
-					}
+		const flagsBig =
+			ImGui.TableFlags.SizingFixedFit |
+			ImGui.TableFlags.RowBg |
+			ImGui.TableFlags.Borders |
+			ImGui.TableFlags.ScrollY |
+			ImGui.TableFlags.NoHostExtendX;
+
+		if (ImGui.BeginTable("disassembly", 4, flagsBig)) {
+			ImGui.TableSetupScrollFreeze(0, 1);
+			ImGui.TableSetupColumn("[PC]", ImGui.TableColumnFlags.WidthFixed);
+			ImGui.TableSetupColumn(
+				"Binary",
+				ImGui.TableColumnFlags.WidthFixed,
+				10 * 8
+			);
+			ImGui.TableSetupColumn(
+				"Instruction",
+				ImGui.TableColumnFlags.WidthFixed,
+				16 * 8
+			);
+			ImGui.TableSetupColumn("Context", ImGui.TableColumnFlags.WidthFixed);
+			ImGui.TableHeadersRow();
+			for (let row = 0; row < 32; row++) {
+				ImGui.TableNextRow();
+				for (let col = 0; col < 4; col++) {
+					ImGui.TableSetColumnIndex(col);
+					ImGui.Text(
+						col === 0
+							? "C000"
+							: col === 1
+							? "4C F5 C5"
+							: col === 2
+							? "JMP $C5F5"
+							: "A:00 X:00 Y:00 P:24 SP:FD CYC:7"
+					);
 				}
-				ImGui.EndTable();
 			}
+			ImGui.EndTable();
 		}
 	}
 }
--- src/gui/components/debugger/Debugger_External.js
+++ src/gui/components/debugger/Debugger_External.js
@@ -12,33 +12,32 @@ export default class Debugger_External {
 			"Select",
 			"Start",
 		];
+		const flags =
+			ImGui.TableFlags.SizingStretchProp |
+			ImGui.TableFlags.RowBg |
+			ImGui.TableFlags.Borders;
+
+		ImGui.Columns(2, "external_controller_columns", false);
+
 		for (let c = 1; c <= 2; c++) {
-			{
-				const flags =
-					ImGui.TableFlags.SizingFixedFit |
-					ImGui.TableFlags.RowBg |
-					ImGui.TableFlags.Borders |
-					ImGui.TableFlags.NoHostExtendX;
-				if (ImGui.BeginTable("controller" + c, 1, flags)) {
-					ImGui.TableSetupColumn(
-						`Controller ${c}`,
-						ImGui.TableColumnFlags.WidthFixed
-					);
-					ImGui.TableHeadersRow();
-					ImGui.TableNextRow();
-					ImGui.TableSetColumnIndex(0);
-					for (let i = 0; i < buttons.length; i++) {
-						const pressed = c === 1 ? i % 2 === 0 : i % 3 === 0; // mock
-						const col = pressed
-							? new ImGui.Vec4(0xc3 / 255, 0x9f / 255, 0x79 / 255, 1) // #c39f79
-							: new ImGui.Vec4(0.5, 0.5, 0.5, 1); // gray
-						ImGui.TextColored(col, buttons[i]);
-						ImGui.SameLine();
-					}
-					ImGui.EndTable();
+			if (ImGui.BeginTable("controller" + c, 1, flags)) {
+				ImGui.TableSetupColumn(`Controller ${c}`, ImGui.TableColumnFlags.None);
+				ImGui.TableHeadersRow();
+				ImGui.TableNextRow();
+				ImGui.TableSetColumnIndex(0);
+				for (let i = 0; i < buttons.length; i++) {
+					const pressed = c === 1 ? i % 2 === 0 : i % 3 === 0;
+					const col = pressed
+						? new ImGui.Vec4(0xc3 / 255, 0x9f / 255, 0x79 / 255, 1)
+						: new ImGui.Vec4(0.5, 0.5, 0.5, 1);
+					ImGui.TextColored(col, buttons[i]);
+					ImGui.SameLine();
 				}
+				ImGui.EndTable();
 			}
-			ImGui.SameLine();
+			ImGui.NextColumn();
 		}
+
+		ImGui.Columns(1);
 	}
 }
