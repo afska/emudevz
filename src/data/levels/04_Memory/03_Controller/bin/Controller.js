2701abb29bab70ad4821daace748b76168710171
Rodrigo Alfonso <rodri042@gmail.com>
2025-09-11 10:07:06 -0300

    Adding controller script

---
+++ src/data/levels/04_Memory/03_Controller/bin/Controller.js
@@ -0,0 +1,35 @@
+const BUTTONS = [
+  "BUTTON_A",
+  "BUTTON_B",
+  "BUTTON_SELECT",
+  "BUTTON_START",
+  "BUTTON_UP",
+  "BUTTON_DOWN",
+  "BUTTON_LEFT",
+  "BUTTON_RIGHT"
+];
+
+export default class Controller {
+  constructor(player) {
+    this.strobe = false;
+    this.cursor = 0;
+    this.other = null;
+
+    this._player = player;
+    this._buttons = [false, false, false, false, false, false, false, false];
+  }
+
+  update(button, isPressed) {
+    const index = BUTTONS.indexOf(button);
+    this._buttons[index] = isPressed;
+  }
+
+  onRead() {
+    /* TODO: IMPLEMENT */
+    return 0;
+  }
+
+  onWrite(value) {
+    /* TODO: IMPLEMENT */
+  }
+}
--- src/data/levels/04_Memory/03_Controller/code/copy.js
+++ src/data/levels/04_Memory/03_Controller/code/copy.js
@@ -1,3 +1,7 @@
 ["controller.en.md", "controller.es.md"].forEach((file) => {
 	filesystem.write(`${Drive.DOCS_DIR}/${file}`, level.bin[file]);
 });
+
+["Controller.js"].forEach((file) => {
+	filesystem.write(`${Drive.TMPL_DIR}/${file}`, level.bin[file]);
+});
